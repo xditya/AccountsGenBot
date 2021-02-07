411a20d8795d2b6fd43d4bf723abdcf929833a14
xditya <58950863+xditya@users.noreply.github.com>
2021-02-07 17:12:32 +0530

    licensing

--- AccountGen/plugins/help.py
+++ AccountGen/plugins/help.py
@@ -1,3 +1,14 @@
+#    AccountsGenBot
+#    Copyright (C) 2021 xditya
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    See < https://github.com/xditya/AccountsGenBot/blob/master/LICENSE > 
+#    for the license.
+
 from . import *
 
 @BotzHub.on(events.callbackquery.CallbackQuery(data="helpme"))
